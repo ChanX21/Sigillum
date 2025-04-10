cff63a73f11a9fe3701cc785eafe608fa3a9053c
codypharm <williamikeji@gmail.com>
2025-04-10 20:52:23 +0100

    nft market landing page

---
+++ Frontend/MainApp/components/nft/NFTCardBrowse.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { NFTCard } from "@/types";
+import Image from "next/image";
+
+interface NFTCardBrowseProps {
+  nft: NFTCard;
+  idx: number;
+}
+
+export const NFTCardBrowse = ({ nft, idx }: NFTCardBrowseProps) => (
+  <div className="w-[320px] rounded-4xl bg-white h-[400px] p-2 flex flex-col gap-2 hover:shadow-lg transition-shadow">
+    <div className="w-full aspect-[16/9] relative overflow-hidden h-[70%]">
+      <Image
+        alt={nft.title}
+        src={nft.image}
+        fill
+        className="rounded-4xl object-cover"
+        priority
+        sizes="(max-width: 768px) 100vw, 320px"
+      />
+    </div>
+
+    <div className="flex h-12 justify-between">
+      <div className="flex items-center gap-2">
+        <div className="h-full flex flex-col justify-center">
+          <p className="text-xl font-medium">{nft.title}</p>
+        </div>
+      </div>
+      {idx % 2 == 0 && (
+        <div className="h-full flex flex-col justify-center">
+          <p className="text-xs text-gray-400">Current bid</p>
+          <p className="text-xs font-semibold">{nft.currentBid} ETH</p>
+        </div>
+      )}
+    </div>
+
+    {idx % 2 == 0 ? (
+      <div className="flex justify-between">
+        <Button variant="default" className="rounded-md w-full cursor-pointer">
+          Place bid
+        </Button>
+      </div>
+    ) : (
+      <div className="flex justify-between">
+        <Button
+          variant="default"
+          className="rounded-md w-[45%]  cursor-pointer"
+        >
+          Place bid
+        </Button>
+        <Button
+          variant="outline"
+          className="rounded-md w-[45%] cursor-pointer border border-primary bg-white"
+        >
+          Instant buy
+        </Button>
+      </div>
+    )}
+  </div>
+);
