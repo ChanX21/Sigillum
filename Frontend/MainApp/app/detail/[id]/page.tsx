450b270b5e42fe41fd8d20d2363088553ee39bf7
codypharm <williamikeji@gmail.com>
2025-04-13 00:52:08 +0100

    chore : mobile responsive on main app

--- Frontend/MainApp/app/detail/[id]/page.tsx
+++ Frontend/MainApp/app/detail/[id]/page.tsx
@@ -9,9 +9,10 @@ export default function Detail() {
     <>
       <Header />
 
-      <main className="flex w-full min-h-screen px-4 md:px-10">
-        <div className="w-2/3 min-h-screen border-r border-stone-300 h-full flex flex-col justify-center items-center">
-          <div className="w-3/4 aspect-[16/9] relative overflow-hidden">
+      <main className="flex flex-col md:flex-row w-full min-h-screen px-4 md:px-10 pt-24">
+        {/* Image Section */}
+        <div className="w-full md:w-2/3 border-b md:border-b-0 md:border-r border-stone-300 flex justify-center items-center py-8 md:py-0">
+          <div className="w-full md:w-3/4 aspect-[16/9] relative overflow-hidden">
             <Image
               alt="nft"
               src="/image.png"
@@ -22,7 +23,9 @@ export default function Detail() {
             />
           </div>
         </div>
-        <div className="w-1/3 min-h-screen h-full p-8 pt-32">
+
+        {/* Detail Section */}
+        <div className="w-full md:w-1/3 h-full p-8 pt-16 md:pt-32">
           <NFTDetailView />
         </div>
       </main>
