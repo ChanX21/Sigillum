1d812d8403ffad234e89587ed0ab886b9b6067d0
Apurvborhade <apurvaborhadee@gmail.com>
2025-05-01 23:06:03 +0530

    session clear

--- Frontend/MainApp/lib/session-disconnect-provider.tsx
+++ Frontend/MainApp/lib/session-disconnect-provider.tsx
@@ -1,20 +1,26 @@
 import { ReactNode, useEffect } from "react";
 import { useWallet } from "@suiet/wallet-kit";
+import { useGetProfile } from "@/hooks/useProfile";
  
 const TIMEOUT_DURATION = 24 * 60 * 60 * 1000; // One day in milliseconds
 
 export const SessionDisconnect = ({children}:{children:ReactNode}) => {
     const {connected,disconnect} =useWallet()
 
+    const { isError: sessionFailed, error: sessionError } = useGetProfile()
+
+    
     useEffect(() => {
         // Trigger disconnection after the timeout period
         const timeoutId = setTimeout(() => {
-          disconnect();
+          if(sessionFailed) {
+            disconnect();
+          }
         }, TIMEOUT_DURATION);
     
         // Clean up the timeout when the component unmounts or when the wallet is connected
         return () => clearTimeout(timeoutId);
-      }, [connected]);
+      }, [connected,sessionFailed]);
     return (
         <>
             {children}
