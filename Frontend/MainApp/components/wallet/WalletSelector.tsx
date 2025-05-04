297cf823bae4c81e33c96ed8f946f5d32a582bb6
codypharm <williamikeji@gmail.com>
2025-05-04 11:27:41 +0100

    double pop up error

--- Frontend/MainApp/components/wallet/WalletSelector.tsx
+++ Frontend/MainApp/components/wallet/WalletSelector.tsx
@@ -1,54 +1,66 @@
-import { useWallet } from '@suiet/wallet-kit'
-import React, { useEffect, useState } from 'react'
-import { Button } from '../ui/button'
-import { useWalletSession } from '@/hooks/useWalletSession'
-import { useGetNonce } from '@/hooks/useSessionAuth'
-import axiosInstance from '@/lib/axios'
-import { toast } from 'sonner'
-import { useGetProfile } from '@/hooks/useProfile'
+import { useWallet } from "@suiet/wallet-kit";
+import React, { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import { useWalletSession } from "@/hooks/useWalletSession";
+import { useGetNonce } from "@/hooks/useSessionAuth";
+import axiosInstance from "@/lib/axios";
+import { toast } from "sonner";
+import { useGetProfile } from "@/hooks/useProfile";
 
 const WalletSelector = () => {
-  const { connected, connecting, address, configuredWallets, allAvailableWallets, detectedWallets, select } = useWallet()
-  const {isSuccess } = useWalletSession()
+  const {
+    connected,
+    connecting,
+    address,
+    configuredWallets,
+    allAvailableWallets,
+    detectedWallets,
+    select,
+  } = useWallet();
+  const { isSuccess } = useWalletSession();
 
-  const [nonce, setNonce] = useState<any>()
+  const [nonce, setNonce] = useState<any>();
 
   const handleWalletClick = async (wallet: any) => {
     if (wallet.installed) {
-      await select(wallet.name)
+      await select(wallet.name);
     } else {
-      console.log(wallet)
+      console.log(wallet);
       // Redirect to wallet website or show a message
       if (wallet.downloadUrl?.browserExtension) {
-        window.open(wallet.downloadUrl.browserExtension, '_blank')
+        window.open(wallet.downloadUrl.browserExtension, "_blank");
       } else {
-        alert(`Please install ${wallet.name} wallet.`)
+        toast(`Please install ${wallet.name} wallet.`);
       }
     }
-  }
-
- 
+  };
 
   useEffect(() => {
     if (isSuccess) {
-      toast.success("User Authenticated Successfully")
+      toast.success("User Authenticated Successfully");
     }
-  }, [isSuccess])
+  }, [isSuccess]);
   return (
-    <div className='relative mt-10 md:px-10 px-15 flex flex-col overflow-y-scroll md:max-h-[85%]'>
-      {[...configuredWallets, ...allAvailableWallets, ...detectedWallets].map((wallet, index) => (
-        <Button
-          key={index}
-          onClick={() => handleWalletClick(wallet)}
-          variant={'ghost'}
-          className="w-full z-50"
-        >
-          <img src={wallet?.iconUrl} alt={wallet.name} className="inline-block w-5 h-5 mr-2" />
-          {wallet.name}
-        </Button>
-      ))}
+    <div className="relative mt-10 md:px-10 px-15 flex flex-col overflow-y-scroll md:max-h-[85%]">
+      {[...configuredWallets, ...allAvailableWallets, ...detectedWallets].map(
+        (wallet, index) => (
+          <Button
+            key={index}
+            onClick={() => handleWalletClick(wallet)}
+            variant={"ghost"}
+            className="w-full z-50"
+          >
+            <img
+              src={wallet?.iconUrl}
+              alt={wallet.name}
+              className="inline-block w-5 h-5 mr-2"
+            />
+            {wallet.name}
+          </Button>
+        )
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default WalletSelector
+export default WalletSelector;
